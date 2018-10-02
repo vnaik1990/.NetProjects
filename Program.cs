6eeabed343deac752539e675a94f7ba1f67dda2b
vnaik1990 <43782929+vnaik1990@users.noreply.github.com>
2018-10-02 18:30:06 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,85 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Replicon_ATMCashWithdrawal
+namespace Replicon_Factorial
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
-            #region dowork
-            test:
-            try
-            {
-                Console.WriteLine("Please enter the amount you wish to withdraw.");
-            int amount = Convert.ToInt32(Console.ReadLine());
-
-            int fivehundredNotes = 0, hundredNotes = 0, fiftyNotes = 0, tenNotes=0, fiveNotes=0;
-            //int amount = 0;
-           
-
-                if (amount % 5 != 0)
-                {
-                    Console.WriteLine("Invalid Amount!");
-                    Console.WriteLine();
-                    goto test;
-                }
-                else
-                {
-                    //check for 500 notes
-                    if (amount >= 500)
-                    {
-                        fivehundredNotes = amount / 500;
-                        amount = amount % 500;
-                    }
-                    //check for 100 notes
-                    if (amount >= 100 && amount != 0)
-                    {
-                        hundredNotes = amount / 100;
-                        amount = amount % 100;
-                    }
-                    //check for 50 notes
-                    if (amount >= 50 && amount != 0)
-                    {
-                        fiftyNotes = amount / 50;
-                        amount = amount % 50;
-                    }
-                    //check for 10 notes
-                    if (amount >= 10 && amount != 0)
-                    {
-                        tenNotes = amount / 10;
-                        amount = amount % 10;
-                    }
-                    //check for 5 notes
-                    if (amount >= 5 && amount != 0)
-                    {
-                        fiveNotes = amount / 5;
-                        amount = amount % 5;
-                    }
-
-                    if (fivehundredNotes > 0)
-                        Console.WriteLine("500 * " + fivehundredNotes);
-                    if (hundredNotes > 0)
-                        Console.WriteLine("100 * " + hundredNotes);
-                    if (fiftyNotes > 0)
-                        Console.WriteLine("50 * " + fiftyNotes);
-                    if (tenNotes > 0)
-                        Console.WriteLine("10 * " + tenNotes);
-                    if (fiveNotes > 0)
-                        Console.WriteLine("5 * " + fiveNotes);
-                    
-                }
-            }
-            catch(Exception ex)
-
-            {
-                Console.WriteLine("Invalid entry.");
-                Console.WriteLine();
-                goto test;
-            }
+            Console.WriteLine("Please enter a number.");
+            int i = Convert.ToInt32(Console.ReadLine());
+            int displayIterNumber = GetFactorial(i);
+            Console.WriteLine("The factorial for the provided number using iteration is :{0}", displayIterNumber);
+            int displayRecNumber = GetFactorialRec(i);
+            Console.WriteLine("The factorial for the provided number using recursion is :{0}", displayRecNumber);
             Console.ReadLine();
-            #endregion
+        }
+
+        public static int GetFactorial(int n)
+        {
+            int fact = 1;
+            for (int j = 1; j <= n; j++)
+            {
+                fact = fact * j;
+            }
+            return fact;
+        }
+
+        public static int GetFactorialRec(int n)
+        {
+            if (n == 0) return 1;
+            else
+                return  n * GetFactorialRec(n - 1);
         }
     }
 }
