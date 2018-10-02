388b77f6d60f52f8b17dca6a74cddb3e82e29d1a
vnaik1990 <43782929+vnaik1990@users.noreply.github.com>
2018-10-02 18:27:14 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Replicon_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            int i = Convert.ToInt32(Console.ReadLine());
+            int displayIterNumber = GetFactorial(i);
+            Console.WriteLine("The factorial for the provided number using iteration is :{0}", displayIterNumber);
+            int displayRecNumber = GetFactorialRec(i);
+            Console.WriteLine("The factorial for the provided number using recursion is :{0}", displayRecNumber);
+            Console.ReadLine();
+        }
+
+        public static int GetFactorial(int n)
+        {
+            int fact = 1;
+            for (int j = 1; j <= n; j++)
+            {
+                fact = fact * j;
+            }
+            return fact;
+        }
+
+        public static int GetFactorialRec(int n)
+        {
+            if (n == 0) return 1;
+            else
+                return  n * GetFactorialRec(n - 1);
+        }
+    }
+}
